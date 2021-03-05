684a639c89db5a9efe323ef9961449acd26e5a19
Fatma Abrougui <fatma.abrougui@esprit.tn>
2021-03-05 01:54:02 +0100

    CRUD Certification

--- Skyway/test/src/Esprit/entities/Certificat.java
+++ Skyway/test/src/Esprit/entities/Certificat.java
@@ -24,50 +24,50 @@ import java.io.Serializable;
 public class Certificat  extends RecursiveTreeObject<Certificat> implements Serializable {
    private int id_certif;
     private String titre_certif;
-    private date date_certif;
+    private String date_certif;
     
 
 
     public Certificat() {
     }
 
-    public Certificat( int id_certif,String titre_certif, int date_certif) {
+    public Certificat( int id_certif,String titre_certif, String date_certif) {
         this.id_certif = id_certif;
         this.titre_certif = titre_certif;
         this.date_certif = date_certif;
     }
-     public Certificat(String titre_certif, int date_certif) {
+     public Certificat(String titre_certif, String date_certif) {
         this.titre_certif = titre_certif;
         this.date_certif = date_certif;
     }
      
 
-    public int getId_q() {
+    public int getId_certif() {
         return id_certif;
     }
 
    
-    public String getText_q() {
+    public String getTitre_certif() {
         return titre_certif;
     }
-public void setId_q(int id_certif) {
+public void setId_certif(int id_certif) {
         this.id_certif = id_certif;
     }
-    public void setText_q(String titre_certif) {
+    public void setTitre_certif(String titre_certif) {
         this.titre_certif = titre_certif;
     }
 
-    public int getNbr_point() {
+    public String getDate_certif() {
         return date_certif;
     }
 
-    public void setNbr_point(int date_certif) {
+    public void setDate_certif(String date_certif) {
         this.date_certif = date_certif;
     }
 
     @Override
     public String toString() {
-        return "Person{" + "id=" + id_certif + ", titre_certif=" + titre_certif + ", nombre de points=" + date_certif + '}';
+        return "Person{" + "id=" + id_certif + ", titre_certif=" + titre_certif + ", date de certification=" + date_certif + '}';
     }
 
     
@@ -75,4 +75,4 @@ public void setId_q(int id_certif) {
     
 }
 
-}
+
