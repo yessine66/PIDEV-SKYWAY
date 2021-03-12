c5878561fa6189433fc725567b2417e889ec558a
ibtihel <ibtihel.awledsoula@esprit.tn>
2021-03-12 13:27:39 +0100

    aa

--- src/Esprit/gui/AddReclamationController.java
+++ src/Esprit/gui/AddReclamationController.java
@@ -5,6 +5,19 @@
  */
 package Esprit.gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author IBTIHEL
+ */
 
 import Esprit.Connection.MyConnection;
 import Esprit.entities.Reclamation;
