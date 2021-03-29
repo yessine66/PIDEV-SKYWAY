ed2f43190c89fea54dd0b8ac626110ec1ce6367f
safa2597 <safa.khouja@esprit.tn>
2021-03-29 16:11:15 +0100

    ..

--- PIDEV-SKYWAY-main/src/Esprit/services/EvenementService.java
+++ PIDEV-SKYWAY-main/src/Esprit/services/EvenementService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Esprit.Services;
+package Esprit.services;
 
 import Esprit.Connection.MyConnection;
 import Esprit.entities.Evenement;
