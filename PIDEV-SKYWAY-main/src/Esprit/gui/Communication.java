3b8c25890a385703b4d95569739f93b10a06219c
ibtihel <ibtihel.awledsoula@esprit.tn>
2021-04-02 03:20:59 +0100

    a

---
+++ PIDEV-SKYWAY-main/src/Esprit/gui/Communication.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Esprit.gui;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * @author IBTIHEL
+ **/
+public class Communication extends Application {
+    
+    @Override
+    public void start(Stage primaryStage)  {
+          try {
+            Parent parent = FXMLLoader.load(getClass().getResource("ReclamationTable.fxml"));
+            Scene scene = new Scene(parent);
+            scene.setFill(Color.TRANSPARENT);
+            primaryStage.setScene(scene);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(Communication.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
